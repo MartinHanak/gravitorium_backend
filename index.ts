5b915fef30437a5d05ac5a9e80f518b06bd0451a
Martin Hanák <martinhanak97@gmail.com>
2023-02-28 13:06:59 +0100

    Change POST to GET method

--- index.ts
+++ index.ts
@@ -2,10 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
 
-/* eslint-disable  @typescript-eslint/no-unsafe-argument */
-// // @ts-expect-error: default fetch
-//const fetch = (...args:any[]) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 const app = express();
 app.use(express.json());
 app.options('*', cors()); // include before other routes
@@ -16,29 +12,30 @@ const corsOptions = {
 
 const PORT = 5000;
 
-app.post('/nasaAPI', cors(corsOptions), (_req, res) => {
-    if (_req.body && _req.body.url && typeof _req.body.url === 'string') {
-        console.log("fetching data");
-        //console.log(_req.body.url);
-        fetch(_req.body.url as string)
+
+app.get('/nasaAPI', cors(corsOptions), (_req, res) => {
+    if(_req.query.url) {
+        const urlNasaAPI = decodeURIComponent(_req.query.url.toString() );
+
+        fetch(urlNasaAPI)
         .then((fetchRes) => {
             return fetchRes.json();
             
         }).then((response) => {
             res.send(response);
-            console.log(response);
         })
         .catch((err) => {
             res.status(400).send(err);
-            console.log(err);
         });
-
+        
     } else {
         res.status(400).send({
-            message: 'Error occured during the NASA API call!'
+            message: 'Error occurred during the NASA API call!'
         });
     }
 });
+
+
 
 app.get('/ping', (_req, res) => {
 console.log('someone pinged here');
